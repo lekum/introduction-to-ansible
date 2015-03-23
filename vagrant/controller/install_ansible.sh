125b0ce36b9c9f133bf7e832efffa0cb6fe50569
Alejandro Guirao <lekumberri@gmail.com>
2015-03-23 19:20:41 +0100

    Add git and cloning to the provisioner script for Vagrant

--- vagrant/controller/install_ansible.sh
+++ vagrant/controller/install_ansible.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
-sudo aptitude install python-pip python-dev -yq
+sudo aptitude install python-pip python-dev git -yq
 sudo pip install ansible
 sudo pip install boto
 sudo mkdir -p /etc/ansible
 sudo cp /vagrant/hosts /etc/ansible/hosts
 sudo cp /vagrant/copy_public_key.sh /home/vagrant
 su vagrant -c 'ssh-keygen -q -t rsa -f ~/.ssh/id_rsa -N ""'
+git clone https://github.com/lekum/introduction-to-ansible.git /home/vagrant/introduction-to-ansible
